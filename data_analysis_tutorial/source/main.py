749b659484c5b4bbaf25d65194f9b9bbe551c970
StefanPenchev05 <comings.belts.0d@icloud.com>
2026-03-16 17:19:51 +0100

    Refactor main.py to comment out data exploration and visualization code

--- data_analysis_tutorial/source/main.py
+++ data_analysis_tutorial/source/main.py
@@ -1,15 +1,35 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 data = pd.read_csv("./houses.csv")
 
 # Gives the first elements from the data frame
-print(data.head())
+#print(data.head())
 
 # Gives information(data types, non-null field... etc.) about the data in the data frame
-print(data.info())
+#print(data.info())
 
 # Gives Important measures - mean, median, min/max .. etc.
-print(data.describe())
+#print(data.describe())
 
 # Investigate missing values
-print(data.isnull().sum())
+#print(data.isnull().sum())
+
+##### Data visualization
+
+# Histogram - reveals Distribution
+# It shows how values are distributed by grouping
+# numbers into ranges
+# plt.hist(data["Price"], bins=20)
+# plt.title("Price Distribution")
+# plt.show()
+
+# plt.scatter(data["Size"], data["Price"])
+# plt.xlabel("Size")
+# plt.ylabel("Price")
+# plt.show()
+
+##### Detecting Missing Data
+
+
+
