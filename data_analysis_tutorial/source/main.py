5dfc154d7b36d8db7274a2f292b340489efbf7db
StefanPenchev05 <comings.belts.0d@icloud.com>
2026-03-16 17:44:18 +0100

    Update main.py to enable data exploration and visualization; uncomment relevant print and plot statements

--- data_analysis_tutorial/source/main.py
+++ data_analysis_tutorial/source/main.py
@@ -4,32 +4,35 @@ import matplotlib.pyplot as plt
 data = pd.read_csv("./houses.csv")
 
 # Gives the first elements from the data frame
-#print(data.head())
+print(data.head())
 
 # Gives information(data types, non-null field... etc.) about the data in the data frame
-#print(data.info())
+print(data.info())
 
 # Gives Important measures - mean, median, min/max .. etc.
-#print(data.describe())
-
-# Investigate missing values
-#print(data.isnull().sum())
+print(data.describe())
 
 ##### Data visualization
 
 # Histogram - reveals Distribution
 # It shows how values are distributed by grouping
 # numbers into ranges
-# plt.hist(data["Price"], bins=20)
-# plt.title("Price Distribution")
-# plt.show()
+plt.hist(data["Price"], bins=20)
+plt.title("Price Distribution")
+plt.show()
 
-# plt.scatter(data["Size"], data["Price"])
-# plt.xlabel("Size")
-# plt.ylabel("Price")
-# plt.show()
+plt.scatter(data["Size"], data["Price"])
+plt.xlabel("Size")
+plt.ylabel("Price")
+plt.show()
 
 ##### Detecting Missing Data
 
+# Investigate missing values
+print(data.isnull().sum())
 
+# Removes missing rows
+data = data.dropna()
 
+# Replace with mean
+data["Bedrooms"].fillna(data["Bedrooms"].mean(), inplace=True)
